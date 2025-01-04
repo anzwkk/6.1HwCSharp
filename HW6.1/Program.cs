bee8c90d62ac55bf54bf1d1b624fd324cb49afa7
anzwkk <anam89737@gmail.com>
2025-01-04 21:45:45 +0300

    Added data entery about employees

--- HW6.1/Program.cs
+++ HW6.1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numOfEmployees;
+string nameOfEmployee;
+double monthSalary;
+
+Console.Write("Введіть кількість працівників: ");
+numOfEmployees = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введіть ім'я кожного працівника");
+
+for (int i = 0; i < numOfEmployees; i++)
+{
+    Console.Write($"Ім'я працівника № {i + 1}: ");
+    nameOfEmployee = Console.ReadLine();
+
+    Console.Write("Введіть зарплату за 1 місяць: ");
+    monthSalary = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Зарплата працівника № {i + 1}, {nameOfEmployee} за 1 місяць ствновить: {monthSalary}");
+}
