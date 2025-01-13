9a9aee6c0c0788c7d1d45dca9364b1dc21b87584
anzwkk <anam89737@gmail.com>
2025-01-13 19:01:25 +0300

    Added calculation of the average earnings of employees

--- HW6.1/Program.cs
+++ HW6.1/Program.cs
@@ -1,6 +1,8 @@
 ﻿int numOfEmployees;
 string nameOfEmployee;
 double monthSalary;
+double totalSalary = 0;
+double averageSalary;
 
 Console.Write("Введіть кількість працівників: ");
 numOfEmployees = Convert.ToInt32(Console.ReadLine());
@@ -14,5 +16,8 @@ for (int i = 0; i < numOfEmployees; i++)
 
     Console.Write("Введіть зарплату за 1 місяць: ");
     monthSalary = Convert.ToDouble(Console.ReadLine());
+    totalSalary += monthSalary;
     Console.WriteLine($"Зарплата працівника № {i + 1}, {nameOfEmployee} за 1 місяць ствновить: {monthSalary}");
 }
+averageSalary = totalSalary / numOfEmployees;
+Console.WriteLine($"Середній заробіток працівників: {averageSalary}");
