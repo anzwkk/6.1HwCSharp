48174023fdcebe2da93e178689905d59cc8c1aa5
anzwkk <anam89737@gmail.com>
2025-01-24 14:40:09 +0300

    added outer loop from 2 to given number

--- HW6.1/Program.cs
+++ HW6.1/Program.cs
@@ -25,15 +25,27 @@
 //Побудова графіку зірочками: Напишіть програму, яка будує графік за допомогою зірочок.
 //Користувач повинен ввести кількість рядків графіка. Кожен рядок графіка має містити відповідну кількість зірочок.
 
-Console.WriteLine("Введіть кількість рядків: ");
-int stringsNum = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введіть кількість рядків: ");
+//int stringsNum = int.Parse(Console.ReadLine());
 
-for (int i = 1; i <= stringsNum; i++)
+//for (int i = 1; i <= stringsNum; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write("*");
+
+//    }
+//    Console.WriteLine()
+//}
+
+//Генерація простих чисел: Напишіть програму, яка генерує прості числа від 1 до заданого користувачем числа. Просте число - це число, яке ділиться лише на 1 і на себе.
+
+Console.WriteLine("Введіть максимальне число:");
+int maxNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Прості числа від 1 до {maxNum}:");
+
+for (int num = 2; num <= maxNum; num++)
 {
-    for (int j = 1; j <= i; j++)
-    {
-        Console.Write("*");
-
-    }
-    Console.WriteLine()
+    bool isPrime = true;
 }
